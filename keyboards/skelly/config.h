695bca07f881bf70520c4ec95d6456c15ee04399
Burkhardt, Shawna <a@b.c>
2023-07-12 15:04:28 -0700

    handwired skeletyl working
    right side matrix is flipped

---
+++ keyboards/skelly/config.h
@@ -0,0 +1,25 @@
+// Copyright 2023 Burkhardt, Shawna (@Burkhardt, Shawna)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+#define SERIAL_PIO_USE_PIO1 // rp2040 only Force the usage of PIO1 peripheral, by default the Serial implementation uses the PIO0 peripheral
+#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
+#define SERIAL_USART_TX_PIN GP0    // USART TX pin NOTE: wires need to be flipped between sides!!
+#define SERIAL_USART_RX_PIN GP1     // USART RX pin
